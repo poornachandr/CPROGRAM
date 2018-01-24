0ee4b6caa6fd8f87a125cac889b114139c9a981a
poornachandr <31505561+poornachandr@users.noreply.github.com>
2018-01-24 16:05:02 +0530

    Create nxt no.c

---
+++ nxt no.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+ int num;
+ printf("\n enter a number");
+ scanf("%d",&num);
+ if(num!=0)
+ {
+  num++;
+  printf("\n the next number is %d",num);
+}
+return 0;
+}
