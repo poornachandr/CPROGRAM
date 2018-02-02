16c13c126784aa552d81c082fb12c9f6eee6b593
poornachandr <31505561+poornachandr@users.noreply.github.com>
2018-02-02 11:09:52 +0000

    Create index.c

---
+++ index.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{ 
+	int n,a[50],i;
+	printf("\n enter the limit:");
+	scanf("%d",&n);
+	printf("\n enter the number one by one");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	printf("\n the number is %d ",a[i]);
+	printf("\n the index value is %d",i);
+  }
+	return 0;
+}
