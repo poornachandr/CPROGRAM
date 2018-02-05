ed07e2878599cc66214b2a344a989bf50decc542
poornachandr <31505561+poornachandr@users.noreply.github.com>
2018-02-05 10:44:57 +0000

    Update numeric.c

--- numeric.c
+++ numeric.c
@@ -6,4 +6,19 @@ int main()
  printf("\n enter the string");
  scanf("%s",&a[i]);
  for(i=0;i<'\0';i++)
- 
+ {
+  if(a[i]=='0'||a[i]=='1'||a[i]=='2'||a[i]=='3'||a[i]=='4'||a[i]=='5'||a[i]=='6'||a[i]=='7'||a[i]=='8'||a[i]=='9')
+  {
+   n=1;
+  }
+ }
+ if(n==1)
+ {
+   printf("\n the string is numeric");
+ }
+ else
+ {
+   printf("\n the string is not numeric");
+ }
+ return 0;
+}
