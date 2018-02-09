3d00d472168e9a5b11720d369001e206d4929fcd
poornachandr <31505561+poornachandr@users.noreply.github.com>
2018-02-09 16:03:53 +0530

    Create strcat.c

---
+++ strcat.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+ char str1[20],str2[20];
+ printf("\n enter first string");
+ scanf("%s",&str1);
+ printf("\n enter second string");
+ scanf("%d",&str2);
+ printf("%s%s",str1,str2);
+ }
+ 
