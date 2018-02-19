7206747efc31dfd6653fc763cd213c3830adf427
poornachandr <31505561+poornachandr@users.noreply.github.com>
2018-02-19 16:52:01 +0530

    Create sortin.c

---
+++ sortin.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+ int i,j,n,t,a[10];
+ printf("\n enter the number:");
+ scanf("%d",&n);
+ for(i=0;i<n;i++)
+ {
+  scanf("%d",&a[i]);
+ }
+ for(i=0;i<n;i++)
+ {
+  for(j=i+1;j<n;j++)
+ {
+  if(a[i]>a[j])
+  {
+   t=a[i];
+   a[i]=a[j];
+   a[j]=t;
+  }
+ }
+}
+for(i=0;i<n;i++)
+{
+printf("%d ",a[i]);
+}
+return 0;
+}
