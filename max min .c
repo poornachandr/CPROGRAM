461105aaebd35f13af84f4d05a594ea5b025786a
poornachandr <31505561+poornachandr@users.noreply.github.com>
2018-02-19 15:46:55 +0530

    Create max min .c

---
+++ max min .c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+ int a[10],i,n,max,min;ax
+ printf("\n enter the number");
+ scanf("%d",&n);
+ for(i=0;i<n;i++)
+ {
+   scanf("%d",&a[i]);
+ }
+ for(i=0;i<n;i++)
+ {
+  printf("\n%d",a[i]);
+ }
+ a[i]=max;
+ for(i=0;i<n;i++)
+ {
+  if(max<a[i])
+  max=a[i];
+  }
+  printf("\n maximum  number is %d",max);
+  min=a[i];
+  for(i=0;i<n;i++)
+  {
+   if(
+  
