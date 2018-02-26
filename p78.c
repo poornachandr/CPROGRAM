65221a91febab4aa5e3c5f924f545b4325ac5049
poornachandr <31505561+poornachandr@users.noreply.github.com>
2018-02-26 16:50:37 +0530

    Create p78.c

---
+++ p78.c
@@ -0,0 +1,16 @@
+include<stdio.h>
+int main()
+{
+ int n;
+ printf("\n enter number");
+ scanf("%d",&n);
+ if(n%13==0)
+ {
+  printf("\n%d is multiple of 13",n);
+ }
+ else
+ {
+ printf(*"\n no");
+ }
+ return 0;
+} 
