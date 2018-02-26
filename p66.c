e5602cf2c3d695e9d508d0d0dd2151a8fdb8f746
poornachandr <31505561+poornachandr@users.noreply.github.com>
2018-02-26 16:08:12 +0530

    Update p66.c

--- p66.c
+++ p66.c
@@ -2,8 +2,8 @@
 int main()
 {
  int n,i,count=0;
- printf("/n enter the number");
- scanrf("%d",&n);
+ printf("\n  enter the number");
+ scanf("%d",&n);
  for(i=1;1<=n;i++)
  {
   if(n%i==0)
@@ -12,11 +12,13 @@ int main()
   }
   if(count==2)
   {
-   printf("/n %d is prime number",n);
+   printf("\n %d is  not  a prime number",n);
+   break;
   }
   else
   {
-   printf("/n %d is not a prime number",n);
+   printf("\n %d is  a prime number",n);
+   break;
   }
  }
  return 0;
