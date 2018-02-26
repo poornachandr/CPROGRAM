66f60d3cbdcca6b234291916fa6f4cd720b1c042
poornachandr <31505561+poornachandr@users.noreply.github.com>
2018-02-26 15:44:08 +0530

    Create p69.c

---
+++ p69.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+ int a,b,c;
+ printf("\n enter two numbers");
+ scanf("%d",&a,&b);
+ if(a>b)
+ {
+  c=a-b;
+  printf("\n the difference is %d",c);
+  }
+  else
+  {
+  c=b-a;
+  printf("\n the difference is %d",c);
+  }
+  if(c%2==0);
+  {
+   printf("\n even");
+   }
+   else
+   {
+    printf("\n odd");
+    }
+    return 0;
+   } 
